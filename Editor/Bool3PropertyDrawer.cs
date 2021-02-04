9fd11b20c08ab60f13ba9eebaada781aee5d6eb4
Adam Graham <adam@zigurous.com>
2021-02-03 19:35:10 -0600

    Add Bool3 struct

---
+++ Editor/Bool3PropertyDrawer.cs
@@ -0,0 +1,72 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Zigurous.DataStructures.Editor
+{
+    [CustomPropertyDrawer(typeof(Bool3))]
+    public sealed class Bool3PropertyDrawer : PropertyDrawer
+    {
+        private SerializedProperty _x;
+        private SerializedProperty _y;
+        private SerializedProperty _z;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (_x == null) { _x = property.FindPropertyRelative("x"); }
+            if (_y == null) { _y = property.FindPropertyRelative("y"); }
+            if (_z == null) { _z = property.FindPropertyRelative("z"); }
+
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            float third = position.width / 3.0f;
+            position.width /= 3.0f;
+            position.width -= 3.0f;
+
+            float labelWidth = EditorGUIUtility.labelWidth;
+            EditorGUIUtility.labelWidth = 10.0f;
+
+            // X
+            EditorGUI.BeginProperty(position, label, _x);
+            {
+                EditorGUI.BeginChangeCheck();
+                bool value = EditorGUI.Toggle(position, "X", _x.boolValue);
+
+                if (EditorGUI.EndChangeCheck()) {
+                    _x.boolValue = value;
+                }
+            }
+            EditorGUI.EndProperty();
+            position.x += third + 1.5f;
+
+            // Y
+            EditorGUI.BeginProperty(position, label, _y);
+            {
+                EditorGUI.BeginChangeCheck();
+                bool value = EditorGUI.Toggle(position, "Y", _y.boolValue);
+
+                if (EditorGUI.EndChangeCheck()) {
+                    _y.boolValue = value;
+                }
+            }
+            EditorGUI.EndProperty();
+            position.x += third + 1.5f;
+
+            // Z
+            EditorGUI.BeginProperty(position, label, _z);
+            {
+                EditorGUI.BeginChangeCheck();
+                bool value = EditorGUI.Toggle(position, "Z", _z.boolValue);
+
+                if (EditorGUI.EndChangeCheck()) {
+                    _z.boolValue = value;
+                }
+            }
+            EditorGUI.EndProperty();
+
+            // Reset editor properties
+            EditorGUIUtility.labelWidth = labelWidth;
+        }
+
+    }
+
+}
---
+++ Runtime/Bool3.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Zigurous.DataStructures
+{
+    [System.Serializable]
+    public struct Bool3 : IEquatable<Bool3>
+    {
+        /// <summary>
+        /// The X component.
+        /// </summary>
+        public bool x;
+
+        /// <summary>
+        /// The Y component.
+        /// </summary>
+        public bool y;
+
+        /// <summary>
+        /// The Z component.
+        /// </summary>
+        public bool z;
+
+        /// <summary>
+        /// Shorthand for writing Bool3(false, false, false).
+        /// </summary>
+        public static Bool3 False => new Bool3(false, false, false);
+
+        /// <summary>
+        /// Shorthand for writing Bool3(true, true, true).
+        /// </summary>
+        public static Bool3 True => new Bool3(true, true, true);
+
+        /// <summary>
+        /// Shorthand for writing Bool3(true, false, false).
+        /// </summary>
+        public static Bool3 X => new Bool3(true, false, false);
+
+        /// <summary>
+        /// Shorthand for writing Bool3(false, true, false).
+        /// </summary>
+        public static Bool3 Y => new Bool3(false, true, false);
+
+        /// <summary>
+        /// Shorthand for writing Bool3(false, false, true).
+        /// </summary>
+        public static Bool3 Z => new Bool3(false, false, true);
+
+        /// <summary>
+        /// Creates a new Bool3 with given x, y, z components.
+        /// </summary>
+        public Bool3(bool x = false, bool y = false, bool z = false)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public bool Equals(Bool3 other)
+        {
+            return this.x == other.x &&
+                   this.y == other.y &&
+                   this.z == other.z;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Bool3 bool3) {
+                return Equals(bool3);
+            } else {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.x.GetHashCode(), this.y.GetHashCode(), this.z.GetHashCode());
+        }
+
+        public static bool operator ==(Bool3 lhs, Bool3 rhs) => lhs.Equals(rhs);
+        public static bool operator !=(Bool3 lhs, Bool3 rhs) => !lhs.Equals(rhs);
+
+        public static Bool3 operator &(Bool3 a, Bool3 b) => new Bool3(
+            a.x & b.x,
+            a.y & b.y,
+            a.z & b.z);
+
+        public static Bool3 operator |(Bool3 a, Bool3 b) => new Bool3(
+            a.x | b.x,
+            a.y | b.y,
+            a.z | b.z);
+
+        public static Bool3 operator ^(Bool3 a, Bool3 b) => new Bool3(
+            a.x ^ b.x,
+            a.y ^ b.y,
+            a.z ^ b.z);
+
+        public static Bool3 operator !(Bool3 a) => new Bool3(
+            !a.x,
+            !a.y,
+            !a.z);
+
+    }
+
+}

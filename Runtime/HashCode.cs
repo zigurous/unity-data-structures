d0968b01e8ed471c109e21b65529e85bccd86c24
Adam Graham <adam@zigurous.com>
2021-02-03 19:35:33 -0600

    Add static HashCode class to combine hashes

---
+++ Runtime/HashCode.cs
@@ -0,0 +1,52 @@
+﻿namespace Zigurous.DataStructures
+{
+    public static class HashCode
+    {
+        /// <summary>
+        /// Combines two hash codes into one unique hash.
+        /// </summary>
+        public static int Combine(int hash1, int hash2)
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 17;
+                hash = hash * 23 + hash1;
+                hash = hash * 23 + hash2;
+                return hash;
+            }
+        }
+
+        /// <summary>
+        /// Combines three hash codes into one unique hash.
+        /// </summary>
+        public static int Combine(int hash1, int hash2, int hash3)
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 17;
+                hash = hash * 23 + hash1;
+                hash = hash * 23 + hash2;
+                hash = hash * 23 + hash3;
+                return hash;
+            }
+        }
+
+        /// <summary>
+        /// Combines four hash codes into one unique hash.
+        /// </summary>
+        public static int Combine(int hash1, int hash2, int hash3, int hash4)
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 17;
+                hash = hash * 23 + hash1;
+                hash = hash * 23 + hash2;
+                hash = hash * 23 + hash3;
+                hash = hash * 23 + hash4;
+                return hash;
+            }
+        }
+
+    }
+
+}

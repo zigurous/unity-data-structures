8d435c63bc027684c07958c32312796cf2e10ffe
Adam Graham <adam@zigurous.com>
2021-02-06 20:25:52 -0600

    Add static Identifier class to generate unique ids

---
+++ Runtime/Identifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Zigurous.DataStructures
+{
+    public static class Identifier
+    {
+        /// <summary>
+        /// Creates a unique identifier based on unix time.
+        /// Since time is always increasing, this value will
+        /// always be different than previous generations.
+        /// </summary>
+        public static long GenerateFromTime()
+        {
+            return new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
+        }
+
+    }
+
+}

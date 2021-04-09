d6e3708d321e8c06d507db63eac8ec94018fc2af
Adam Graham <adam@zigurous.com>
2021-04-09 14:31:41 -0500

    Add RenameAttribute property attribute and drawer

---
+++ Editor/Attributes/RenameAttribute.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class RenameAttribute : PropertyAttribute
+{
+    public string newName;
+
+    public RenameAttribute(string newName)
+    {
+        this.newName = newName;
+    }
+
+}
+
+[CustomPropertyDrawer(typeof(RenameAttribute))]
+public class RenamePropertyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.PropertyField(position, property, new GUIContent((attribute as RenameAttribute).newName));
+    }
+
+}

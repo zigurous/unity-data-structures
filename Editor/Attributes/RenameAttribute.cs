c5634d435c8dcbc167ee5d86fb1448fe4d212d28
Adam Graham <adam@zigurous.com>
2021-04-12 18:30:27 -0500

    Add missing namespace to RenameAttribute

--- Editor/Attributes/RenameAttribute.cs
+++ Editor/Attributes/RenameAttribute.cs
@@ -1,23 +1,27 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-public class RenameAttribute : PropertyAttribute
+namespace Zigurous.DataStructures.Editor
 {
-    public string newName;
-
-    public RenameAttribute(string newName)
+    public class RenameAttribute : PropertyAttribute
     {
-        this.newName = newName;
+        public string newName;
+
+        public RenameAttribute(string newName)
+        {
+            this.newName = newName;
+        }
+
     }
 
-}
-
-[CustomPropertyDrawer(typeof(RenameAttribute))]
-public class RenamePropertyDrawer : PropertyDrawer
-{
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    [CustomPropertyDrawer(typeof(RenameAttribute))]
+    public class RenamePropertyDrawer : PropertyDrawer
     {
-        EditorGUI.PropertyField(position, property, new GUIContent((attribute as RenameAttribute).newName));
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.PropertyField(position, property, new GUIContent((attribute as RenameAttribute).newName));
+        }
+
     }
 
 }

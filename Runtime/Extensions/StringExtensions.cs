047ebb6c2acc9547b4999f4cf7dbabfa0288f17c
Adam Graham <adam@zigurous.com>
2021-07-12 23:50:06 -0500

    Remove string extensions for abbreviating numbers

--- Runtime/Extensions/StringExtensions.cs
+++ Runtime/Extensions/StringExtensions.cs
@@ -1,5 +1,4 @@
 using System.Text;
-using UnityEngine;
 
 namespace Zigurous.DataStructures
 {
@@ -8,48 +7,6 @@ namespace Zigurous.DataStructures
     /// </summary>
     public static class StringExtensions
     {
-        /// <summary>
-        /// Represents how a number is abbreviated to a string.
-        /// </summary>
-        [System.Serializable]
-        public struct NumberAbbreviation
-        {
-            /// <summary>
-            /// The number factor after which the abbreviation is applied.
-            /// </summary>
-            [Tooltip("The number factor after which the abbreviation is applied.")]
-            public float factor;
-
-            /// <summary>
-            /// The string format of the abbreviated number.
-            /// </summary>
-            [Tooltip("The string format of the abbreviated number.")]
-            public string format;
-
-            /// <summary>
-            /// Creates a new number abbreviation with the given
-            /// <paramref name="factor"/> and <paramref name="format"/>.
-            /// </summary>
-            /// <param name="factor">The number factor after which the abbreviation is applied.</param>
-            /// <param name="format">The string format of the abbreviated number.</param>
-            public NumberAbbreviation(float factor, string format)
-            {
-                this.factor = factor;
-                this.format = format;
-            }
-
-        }
-
-        /// <summary>
-        /// A pre-defined set of number abbreviations.
-        /// </summary>
-        private static readonly NumberAbbreviation[] abbreviations = new NumberAbbreviation[4] {
-            new NumberAbbreviation(1_000_000_000_000, "0T"),
-            new NumberAbbreviation(1_000_000_000, "0B"),
-            new NumberAbbreviation(1_000_000, "0M"),
-            new NumberAbbreviation(1_000, "0K"),
-        };
-
         /// <summary>
         /// A reusable string builder.
         /// </summary>
@@ -90,39 +47,6 @@ namespace Zigurous.DataStructures
             stringBuilder.Capacity = str.Length * n;
             stringBuilder.Insert(0, str, n);
             return stringBuilder.ToString();
-        }
-
-        /// <summary>
-        /// Converts the number to an abbreviated string.
-        /// </summary>
-        /// <param name="number">The number to abbreviate.</param>
-        /// <returns>A new string of the abbreviated number.</returns>
-        public static string ToAbbreviatedString(this float number)
-        {
-            return ToAbbreviatedString(number, StringExtensions.abbreviations);
-        }
-
-        /// <summary>
-        /// Converts the number to an abbreviated string using the provided
-        /// <paramref name="abbreviations"/>.
-        /// </summary>
-        /// <param name="number">The number to abbreviate.</param>
-        /// <param name="abbreviations">The abbreviations that can be applied to the number.</param>
-        /// <returns>A new string of the abbreviated number.</returns>
-        public static string ToAbbreviatedString(this float number, NumberAbbreviation[] abbreviations)
-        {
-            float abs = System.Math.Abs(number);
-
-            for (int i = 0; i < abbreviations.Length; i++)
-            {
-                NumberAbbreviation abbreviation = abbreviations[i];
-
-                if (abs >= abbreviation.factor) {
-                    return (number / abbreviation.factor).ToString(abbreviation.format);
-                }
-            }
-
-            return number.ToString();
         }
 
     }

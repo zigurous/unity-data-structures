c2f9046f7d1758bc5f057879e8d7012392666559
Adam Graham <adam@zigurous.com>
2022-05-11 17:01:47 -0500

    Move INumberRange into its own file

---
+++ Runtime/Structs/INumberRange.cs
@@ -0,0 +1,33 @@
+﻿namespace Zigurous.DataStructures
+{
+    /// <summary>
+    /// A generic interface for a range of number values.
+    /// </summary>
+    /// <typeparam name="T">The type of values in the range.</typeparam>
+    public interface INumberRange<T> : IRange<T>
+    {
+        /// <summary>
+        /// The difference between the maximum and minimum values (Read only).
+        /// </summary>
+        T delta { get; }
+
+        /// <summary>
+        /// The median value of the range (Read only).
+        /// </summary>
+        T median { get; }
+
+        /// <summary>
+        /// Returns a random value in the range.
+        /// </summary>
+        /// <returns>A random value in the range.</returns>
+        T Random();
+
+        /// <summary>
+        /// Clamps a value to the range.
+        /// </summary>
+        /// <param name="value">The value to clamp.</param>
+        /// <returns>The clamped value.</returns>
+        T Clamp(T value);
+    }
+
+}
